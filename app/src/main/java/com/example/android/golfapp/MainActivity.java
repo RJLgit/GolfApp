6db38ef1962c502a4fe24899f13631bd28b5b96a
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-08 10:18:45 +0100

    Clicks on the bottom navbar cycles through fragments as required

--- app/src/main/java/com/example/android/golfapp/MainActivity.java
+++ app/src/main/java/com/example/android/golfapp/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.android.golfapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+    //UI elements
+    FrameLayout container;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        container = findViewById(R.id.fragment_container);
+        bottomNavigationView = findViewById(R.id.bott_nav_bar);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment myFragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_enter:
+                myFragment = new EnterFragment();
+                break;
+            case R.id.nav_list:
+                myFragment = new ListFragment();
+                break;
+            case R.id.nav_stats:
+                myFragment = new StatsFragment();
+                break;
+        }
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, myFragment).commit();
+        return true;
     }
 }
