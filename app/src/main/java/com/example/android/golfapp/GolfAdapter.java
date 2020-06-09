c7185c4ab07dea396f258364cd8c49f489b80c6f
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-09 09:10:29 +0100

    Created and set an adapter and to the recycler view

---
+++ app/src/main/java/com/example/android/golfapp/GolfAdapter.java
@@ -0,0 +1,70 @@
+package com.example.android.golfapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.android.golfapp.Data.GolfRecord;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GolfAdapter extends RecyclerView.Adapter<GolfAdapter.GolfViewHolder> {
+
+    private ArrayList<GolfRecord> mData = new ArrayList<>();
+    private Context mContext;
+
+    public GolfAdapter(Context context) {
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public GolfViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        View view = inflater.inflate(R.layout.fragment_list_item, parent, false);
+        return new GolfViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GolfViewHolder holder, int position) {
+        holder.nameTextView.setText(mData.get(position).getName());
+        holder.courseTextView.setText(mData.get(position).getCourse());
+        holder.dateTextView.setText(mData.get(position).getDate().toString());
+        holder.parTextView.setText(mData.get(position).getPar());
+        holder.scoreTextView.setText(mData.get(position).getScore());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public void setmData(ArrayList<GolfRecord> mData) {
+        this.mData = mData;
+    }
+
+    class GolfViewHolder extends RecyclerView.ViewHolder {
+        TextView dateTextView;
+        TextView nameTextView;
+        TextView courseTextView;
+        TextView parTextView;
+        TextView scoreTextView;
+
+
+        public GolfViewHolder(@NonNull View itemView) {
+            super(itemView);
+            dateTextView = itemView.findViewById(R.id.date_item_text_view);
+            nameTextView = itemView.findViewById(R.id.name_item_text_view);
+            courseTextView = itemView.findViewById(R.id.course_item_text_view);
+            parTextView = itemView.findViewById(R.id.par_item_text_view);
+            scoreTextView = itemView.findViewById(R.id.score_item_text_view);
+
+        }
+    }
+}
--- app/src/main/java/com/example/android/golfapp/ListFragment.java
+++ app/src/main/java/com/example/android/golfapp/ListFragment.java
@@ -4,6 +4,8 @@ package com.example.android.golfapp;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +16,7 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class ListFragment extends Fragment {
-
+    RecyclerView myRecyclerView;
 
     public ListFragment() {
         // Required empty public constructor
@@ -25,7 +27,16 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View v = inflater.inflate(R.layout.fragment_list, container, false);
+
+        myRecyclerView = v.findViewById(R.id.recyclerview);
+        myRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        GolfAdapter adapter = new GolfAdapter(getContext());
+        myRecyclerView.setAdapter(adapter);
+
+
+        return v;
     }
 
 }
