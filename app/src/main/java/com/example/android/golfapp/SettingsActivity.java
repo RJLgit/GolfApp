e237ea503ea1d7ece46313c144666581e0329d2a
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-10 16:12:57 +0100

    Set up settings activity with filter for data range

--- app/src/main/java/com/example/android/golfapp/SettingsActivity.java
+++ app/src/main/java/com/example/android/golfapp/SettingsActivity.java
@@ -1,14 +1,37 @@
 package com.example.android.golfapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NavUtils;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        toolbar = findViewById(R.id.toolbar);
+
+
+        setSupportActionBar(toolbar);
+        toolbar.setLogo(R.drawable.ic_app_icon);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setSubtitle("Change which data to see");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            //Navigates up to MainActivity using this method
+            NavUtils.navigateUpFromSameTask(this);
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
