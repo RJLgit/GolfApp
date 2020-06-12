f304712f33990f9b6bde12d36fde627ae796df9a
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-12 10:07:56 +0100

    Removed sort and filter from stats page

--- app/src/main/java/com/example/android/golfapp/StatsFragment.java
+++ app/src/main/java/com/example/android/golfapp/StatsFragment.java
@@ -1,6 +1,7 @@
 package com.example.android.golfapp;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,23 +10,46 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.golfapp.Data.GolfDatabase;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class StatsFragment extends Fragment {
 
+    GolfDatabase mDb;
 
     public StatsFragment() {
         // Required empty public constructor
     }
 
+    public StatsFragment(GolfDatabase mDb) {
+        this.mDb = mDb;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stats, container, false);
+        View v = inflater.inflate(R.layout.fragment_stats, container, false);
+
+
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof MainActivity) {
+            MainActivity activity = (MainActivity) context;
+            activity.hideMenus();
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement EnterFragmentListener");
+        }
+
     }
 
 }
