875a8c6bb7722a1e07e7c9afbdd6cedddfe027e2
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-12 10:23:39 +0100

    Created spinner to contain the names of the players

--- app/src/main/java/com/example/android/golfapp/StatsFragment.java
+++ app/src/main/java/com/example/android/golfapp/StatsFragment.java
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.android.golfapp.Data.GolfDatabase;
 
@@ -19,6 +21,8 @@ import com.example.android.golfapp.Data.GolfDatabase;
 public class StatsFragment extends Fragment {
 
     GolfDatabase mDb;
+
+    Spinner spinner;
 
     public StatsFragment() {
         // Required empty public constructor
@@ -33,6 +37,13 @@ public class StatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_stats, container, false);
+        spinner = v.findViewById(R.id.stats_spinner);
+
+        String names[] = {"Bob","Tom","Jeff"};
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, names);
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item); // The drop down view
+        spinner.setAdapter(spinnerArrayAdapter);
 
 
 
