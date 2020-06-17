6b7fb249a05d07f3ad413152df377be993baf9dd
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-17 05:53:58 +0100

    Added comments to GolfDao

--- app/src/main/java/com/example/android/golfapp/Data/GolfDao.java
+++ app/src/main/java/com/example/android/golfapp/Data/GolfDao.java
@@ -7,26 +7,30 @@ import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
-
+//Room Dao interface for interacting with the db
 @Dao
 public interface GolfDao {
+    //Gets all the records in the db
     @Query("SELECT * FROM golfscores ORDER BY date")
     LiveData<List<GolfRecord>> loadAllRecords();
 
+    //Gets a single players records from the db
     @Query("SELECT * FROM golfscores WHERE name LIKE :name ORDER BY date")
     LiveData<List<GolfRecord>> loadPersonRecords(String name);
 
-    //Can make list a set to remove duplicates later
+    //Gets all the names in the db
     @Query("SELECT name from golfscores")
     LiveData<List<String>> getAllNames();
 
-    //Can make list a set to remove duplicates later
+    //Gets all the courses in the db
     @Query("SELECT course from golfscores")
     LiveData<List<String>> getAllCourses();
 
+    //Inserts a record into the db
     @Insert
     void insertGolfRecord(GolfRecord golfRecord);
 
+    //Deletes a record from the db
     @Delete
     void deleteTask(GolfRecord golfRecord);
 }
