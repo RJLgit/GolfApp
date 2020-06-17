4e94fef2aba59cabb07abf749d2a60e5d7210f97
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-17 08:16:44 +0100

    Added comments and extracted strings from SettingsFragment

--- app/src/main/java/com/example/android/golfapp/SettingsFragment.java
+++ app/src/main/java/com/example/android/golfapp/SettingsFragment.java
@@ -6,6 +6,8 @@ import androidx.preference.MultiSelectListPreference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
+
+    //This variable holds all the distinct player names held in the database
     String[] myNames;
     public SettingsFragment(String[] names) {
         myNames = names;
@@ -14,8 +16,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_golf);
-
-        MultiSelectListPreference multiSelectListPreference = findPreference("player_filter_preference");
+        //Sets the data to the preference to select the players. This has to be done dynamically as it responds to the database changing
+        MultiSelectListPreference multiSelectListPreference = findPreference(getString(R.string.preference_player_filter_key));
         multiSelectListPreference.setEntries(myNames);
         multiSelectListPreference.setEntryValues(myNames);
         multiSelectListPreference.setDefaultValue(myNames);
