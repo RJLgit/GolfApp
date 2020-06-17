f16bc87e60bdd38caffc2e52f0f1b968c1f2ad0c
Rich <r.j.lewindon@hotmail.co.uk>
2020-06-17 05:55:50 +0100

    Added comments to db class

--- app/src/main/java/com/example/android/golfapp/Data/GolfDatabase.java
+++ app/src/main/java/com/example/android/golfapp/Data/GolfDatabase.java
@@ -2,13 +2,11 @@ package com.example.android.golfapp.Data;
 
 import android.content.Context;
 import android.util.Log;
-
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
-import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
-
+//Singleton pattern used in this class to creat the room database
 @Database(entities = {GolfRecord.class}, version = 1, exportSchema = false)
 @TypeConverters(DateConverter.class)
 public abstract class GolfDatabase extends RoomDatabase {
@@ -17,18 +15,14 @@ public abstract class GolfDatabase extends RoomDatabase {
     private static final Object LOCK = new Object();
     private static final String DATABASE_NAME = "golfdatabase";
     private static GolfDatabase mInstance;
-
+    //Get instance method to ensure only a single database instance exists
     public static GolfDatabase getInstance(Context context) {
         if (mInstance == null) {
+            //Stops 2 db objects being made at the same time
             synchronized (LOCK) {
                 Log.d(TAG, "Creating new db instance");
                 mInstance = Room.databaseBuilder(context.getApplicationContext(),
                         GolfDatabase.class, GolfDatabase.DATABASE_NAME)
-                       /* !!!!!!!!!!!!!
-                                !!!!!!!!!!!!!
-                                        !!!!!!!!!!!!
-                                                Need to change this*/
-                        //.allowMainThreadQueries()
                         .build();
             }
         }
